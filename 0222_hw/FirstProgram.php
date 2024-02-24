4435222831741d0335a853b6a945f873ff93b9b2
willhuang123 <160864392+willhuang123@users.noreply.github.com>
2024-02-24 10:55:43 +0800

    Add files via upload

---
+++ 0222_hw/FirstProgram.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+</head>
+<body>
+    <?php
+        echo "Hello";
+        echo "<br/>";
+        echo "A1113302";
+
+        $a = 0;
+        $b = 1;
+        
+        if($a != $b)
+        {
+            echo " test";
+        }
+    ?>
+</body>
+</html>
